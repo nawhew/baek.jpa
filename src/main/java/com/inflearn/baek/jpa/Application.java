8cd6bfae409956787fe2996bbc20ea6814b9889a
wehwan <lxmfhvll@gmail.com>
2020-09-13 21:20:02 +0900

    Application 초기 세팅 추가

--- src/main/java/com/inflearn/baek/jpa/Application.java
+++ src/main/java/com/inflearn/baek/jpa/Application.java
@@ -1,16 +1,20 @@
 package com.inflearn.baek.jpa;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication springApplication = new SpringApplication(Application.class);
 		springApplication.setWebApplicationType(WebApplicationType.NONE);
-		springApplication.run(args);
+		//springApplication.run(args);
+
+		log.info("Run Jpa Application!!");
 	}
 
 }
